175e129d94f9174e93da9b3383eb1cd477835e49
Anton Kazaryan <kazarian.anton.wp@mail.ru>
2024-10-03 10:50:32 +0300

    add tailwind

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
   return (
-    <div>
-
+    <div className="bg-blue-500 text-white p-4">
+      Если фон синий — Tailwind работает!
     </div>
-  )
-}
- 
-export default App
+  );
+};
+
+export default App;
